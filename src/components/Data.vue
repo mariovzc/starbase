c39d23d4226fdcc9d8956fabf542282f0509a717
Mario vizcaino <mario.vizcainos@gmail.com>
2017-11-23 11:53:28 -0500

    display all the data

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -1,22 +1,36 @@
 <template>
   <div>
-    Type: {{type}}
+    {{items}}
   </div>
 </template>
 <script>
 export default {
   data () {
     return {
-      type: this.$route.params.type
+      type: this.$route.params.type,
+      items: []
     }
   },
   watch: {
-    '$route': 'change'
+    '$route': 'fetchItems'
   },
   methods: {
-    change () {
+    fetchItems () {
+      this.items = []
       this.type = this.$route.params.type
+      let initial_ids = [1, 13, 14]
+      initial_ids.forEach(id => {
+        console.log(id) 
+        fetch(`https://swapi.co/api/${this.type}/${id}`,{
+          method: 'GET'
+        })
+        .then(response => response.json())
+        .then( json => this.items.push(json))
+      })
     }
+  },
+  created() {
+    this.fetchItems()
   }
 }
 </script>
