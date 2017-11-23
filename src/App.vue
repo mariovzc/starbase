a16b2e5716a661990312ea7c85ba8583f341128b
Mario vizcaino <mario.vizcainos@gmail.com>
2017-11-23 10:54:28 -0500

    created characters component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="app">
     <h3>{{ title }}</h3>
+    <Character/>
   </div>
 </template>
 <script>
+import Character from './components/Character.vue'
+
 export default {
   name: 'app',
   data() {
     return {
       title: 'Generate your team'
     }
+  },
+  components: {
+    Character
   }
 }
 </script>
---
+++ src/components/Character.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{character}}    
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      character: {}
+    }
+  },
+  methods: {
+    fetchCharacter() {
+      fetch('https://swapi.co/api/people', {
+        method: 'GET'
+      })
+      .then(response => response.json())
+      .then(json => this.character = json)
+    }
+  },
+  created () {
+    this.fetchCharacter()
+  }
+}
+</script>
