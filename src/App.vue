330108bf502fd9e3e546c65b1731201427cda4fb
Mario vizcaino <mario.vizcainos@gmail.com>
2017-11-23 11:03:02 -0500

    display all the 3 characters

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
     <h3>{{ title }}</h3>
-    <Character/>
+    <div class="col-md-12">
+      <Character 
+        v-for="(id, index) in initial_ids" 
+        :id="id"
+        key = "index"
+        />
+    </div>
   </div>
 </template>
 <script>
@@ -11,7 +17,8 @@ export default {
   name: 'app',
   data() {
     return {
-      title: 'Generate your team'
+      title: 'Generate your team',
+      initial_ids: [1,13,14]
     }
   },
   components: {
